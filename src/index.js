fa90b088e0d79a8d5f3f5aceacc096c3adc72de7
ky-peace <101023575+ky-peace@users.noreply.github.com>
2022-03-08 17:05:40 -0500

    Copied over html js css and json from codesandbox

---
+++ src/index.js
@@ -0,0 +1,109 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#current-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#low").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+  document.querySelector("#high").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#conditions").innerHTML =
+    response.data.weather[0].main;
+}
+
+function search(city) {
+  let apiKey = "dd66198ca4a46c65380b73f0c31de66e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+  console.log(apiUrl);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#new-city-value").value;
+  search(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "dd66198ca4a46c65380b73f0c31de66e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function displayFahrenheit(response) {
+  document.querySelector("#current-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#new-city-value").value;
+  let apiKey = "dd66198ca4a46c65380b73f0c31de66e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayFahrenheit);
+}
+
+function displayCelsius(response) {
+  document.querySelector("#current-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+function convertToCelsius(event) {
+  event.preventDefault();
+  let city = document.querySelector("#new-city-value").value;
+  let apiKey = "dd66198ca4a46c65380b73f0c31de66e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayCelsius);
+}
+
+let dateElement = document.querySelector("#current-date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#search-city-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+let celsiusLink = document.querySelector("#tempc-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+let fahrenheitLink = document.querySelector("#tempf-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+search("Toronto");
